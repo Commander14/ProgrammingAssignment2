abe5d0c95091ac1e945a0f05934d53ede7493b68
Commander14 <toonse@gmail.com>
2014-07-26 10:52:38 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,11 @@ makeCacheMatrix <- function(x = matrix()) {
         stop("You can only invert a square matrix!")
     }
 
+    ## default the cached value to NULL
     inverse <- NULL
+
+    ## setup functions for the cacheSolve function to use to introspect whether there
+    ## are cached results or not and to set/get them
     set <- function(y) {
         x <<- y
         inverse <<- NULL
@@ -16,6 +20,8 @@ makeCacheMatrix <- function(x = matrix()) {
     get <- function() x
     setSolve <- function(solve) inverse <<- solve
     getSolve <- function() inverse 
+
+    ## expose the internal functions to the parent of this object
     list(set = set, get = get,
          setSolve = setSolve,
          getSolve = getSolve)
@@ -25,44 +31,22 @@ makeCacheMatrix <- function(x = matrix()) {
 ## A solver that will return the inversion of a matrix, and if that particular
 ## inversion has been seen before it will return a cached result
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
+    ## fetch what we think might be a cached result, but might be NULL as well
     inverse <- x$getSolve()
+
+    ## if we have a cached result, then by all means return it!
     if(!is.null(inverse)) {
         message("getting cached data")
         return(inverse)
     }
+
+    ## Haven't done this yet? Calculate the inversion
     data <- x$get()
     inverse <- solve(data, ...)
+
+    ## cache the result in the passed in cacheMatrix cache
     x$setSolve(inverse)
+
+    ## return the result
     inverse
-}
-
-## test function borrowed from Subramanian Kartik on the discussion boards
-testFunc <- function()
-{
-    ## First I create a test matrix to invert...
-    a<-matrix(data=1:4,nrow=2,ncol=2)
-    print("Matrix to be inverted:")
-    print(a)
-
-    ## As cacheSolve() is expecting type makeCacheMatrix, use the constructor to create z from a
-    z<-makeCacheMatrix(a)
-
-    ## Now call cacheSolve() 5 times on z - 1st time should have no cached inverse, but subsequent calls should
-    ## 5 times is arbitrary..2 should be enough
-    print("1...Should not have cached data")
-    zinv<-cacheSolve(z)
-    print(zinv)
-    
-    print("2...Cached Data")
-    zinv<-cacheSolve(z)
-    print(zinv)
-
-    print("3...Cached Data")
-    zinv<-cacheSolve(z)
-    print(zinv)
-
-    zinv<-cacheSolve(z)
-    print("Final result - Inverse Matrix:")
-    print(zinv)
 }
